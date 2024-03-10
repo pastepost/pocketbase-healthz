db5b051f11c7503c79d4779a1eff1b3f3840981d
ilyabumblebee <hipomotgames@gmail.com>
2024-03-10 21:58:30 +0200

    ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apps : [{
+        name   : "pbapi",
+        script : "pnpm start",
+        env_production: {
+        NODE_ENV: "production",
+        POCKETBASE_URL: "http://10.114.0.3"
+        }
+    }]
+}
